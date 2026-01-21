75638ec4cc3d0d91dadba63b98a69d512277a26d
Alongkorn.py <92965145+Alongkron1234@users.noreply.github.com>
2026-01-21 21:08:45 +0700

    Edit2

--- frontend/app/utils/lib/axios.js
+++ frontend/app/utils/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000', // ใส่ URL ของ Node.js Server ของคุณที่นี่
+  baseURL: 'http://localhost:8000',
 });
 
 export default api;
